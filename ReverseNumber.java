ee9d0a7b9c70a91e96f0e31017cc2f67267fdd8a
ANIKET DHAKITE <aniketdhakite33@gmail.com>
2026-02-17 23:45:33 +0530

    Reverse Number

---
+++ ReverseNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class ReverseNumber
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+
+        String str = num+"";
+        String rev ="";
+
+        for(int i=0; i<str.length();i++)
+        {
+            rev = str.charAt(i) + rev;
+
+        }
+
+        System.out.print(rev);
+    }
+}
+
